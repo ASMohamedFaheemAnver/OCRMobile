e098bcf38a571ef2cc7498157d8f8470791d8c6f
ubunturifa <jstrfaheem065@gmail.com>
2019-12-23 00:01:21 +0530

    Crop picture,

--- app/src/main/java/com/flover/ocrapplication/MainActivity.kt
+++ app/src/main/java/com/flover/ocrapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
 import android.database.Cursor
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -20,9 +21,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.loopj.android.http.RequestParams
 import com.loopj.android.http.SyncHttpClient
 import com.loopj.android.http.TextHttpResponseHandler
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import cz.msebera.android.httpclient.Header
 import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.security.AccessController.getContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -81,6 +86,11 @@ class MainActivity : AppCompatActivity() {
             }.start()
         }
 
+        findViewById<Button>(R.id.camera_btn).setOnClickListener {
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 2)
+        }
+
         textView = findViewById(R.id.show_result)
         textView.text = ""
         progressDialog = ProgressDialog(this)
@@ -90,8 +100,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        progressDialog.show()
         if (requestCode==1&&resultCode== Activity.RESULT_OK&&data!=null){
+            progressDialog.show()
             var uri: Uri? = data.data
             findViewById<ImageView>(R.id.uploaded_image).setImageURI(uri)
 
@@ -132,6 +142,66 @@ class MainActivity : AppCompatActivity() {
                 })
                 Looper.loop()
             }.start()
+        }else if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE&&resultCode== Activity.RESULT_OK&&data!=null){
+
+            progressDialog.show()
+            var result = CropImage.getActivityResult(data)
+            var uri = result.uri
+
+            findViewById<ImageView>(R.id.uploaded_image).setImageURI(uri)
+
+
+            var client = SyncHttpClient()
+            var params = RequestParams()
+
+            path = uri?.let { getPath(baseContext, it) }!!
+
+            params.put("image", File(path), path?.let { getContentType(it) })
+            // params.put("result", "GOOGLE!")
+            client.connectTimeout = 60000
+            client.responseTimeout = 120000
+
+            Thread{
+                Looper.prepare()
+                client.post("http://$localhost:3000/api/result", params, object : TextHttpResponseHandler(){
+                    override fun onSuccess(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        responseString: String?
+                    ) {
+                        val json = JSONObject(responseString)
+                        string = json["image_text"].toString()
+                        this@MainActivity.runOnUiThread {
+                            textView.text = string
+                            progressDialog.dismiss()
+                        }
+                    }
+
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        responseString: String?,
+                        throwable: Throwable?
+                    ) {
+                        println(responseString)
+                    }
+                })
+                Looper.loop()
+            }.start()
+        }else if(requestCode==2&&resultCode== Activity.RESULT_OK&&data!=null){
+//            var cropIntent = Intent("com.android.camera.action.CROP")
+//            cropIntent.setDataAndType(data?.data, "image/*")
+//            cropIntent.putExtra("crop", true)
+//            intent.putExtra("outputX", 512)
+//            intent.putExtra("outputY", 512)
+//            intent.putExtra("aspectX", 1)
+//            intent.putExtra("aspectY", 1)
+//            cropIntent.putExtra("return-data", true)
+//            startActivityForResult(cropIntent, 3)
+
+            CropImage.activity(data?.data)
+                .start(this)
+
         }
     }
 
@@ -158,12 +228,16 @@ class MainActivity : AppCompatActivity() {
                 val split = docId.split(":").toTypedArray()
                 val type = split[0]
                 var contentUri: Uri? = null
-                if ("image" == type) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                } else if ("video" == type) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                } else if ("audio" == type) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                when (type) {
+                    "image" -> {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    }
+                    "video" -> {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    }
+                    "audio" -> {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                    }
                 }
                 val selection = "_id=?"
                 val selectionArgs = arrayOf(
